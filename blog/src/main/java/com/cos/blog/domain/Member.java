5c891d22a4f13ed15a26f7da60bf01da6ea708fd
KO_HANGANG <rhgksrkd@naver.com>
2023-08-01 16:44:54 +0900

    blog commit

---
+++ blog/src/main/java/com/cos/blog/domain/Member.java
@@ -0,0 +1,18 @@
+package com.cos.blog.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name="member_id")
+    private Long id;
+
+    private String name;
+
+}
